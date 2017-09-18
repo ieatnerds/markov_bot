1434e58de8aacd03c005940806e2c56301f831d1
Nick Kelly <importantnk@gmail.com>
2017-09-17 20:38:15 -0400

    Initial commit.

---
+++ markov.py
@@ -0,0 +1,13 @@
+import markovify
+
+
+def message():
+    # Get raw text as string.
+    with open("big.txt") as f:
+        text = f.read()
+
+    # Build the model.
+    text_model = markovify.Text(text)
+
+    # Print three randomly-generated sentences of no more than 140 characters
+    return text_model.make_short_sentence(140)
---
+++ markov_bot.py
@@ -0,0 +1,48 @@
+"""
+Author: importantnk@gmail.com
+Rath Twitter Bot
+
+This is a simple bot for twitter that is being used for several functions.
+The main function of the bot is to provide an easy learning experience with
+the Twitter API and pulling information from other sources to post on twitter.
+"""
+# Imports
+import markov
+import logging
+import sys
+from make_dir import makeDir
+from twython import Twython
+from auth import (consumer_key,
+                  consumer_secret,
+                  access_token,
+                  access_token_secret,
+                  )
+
+twitter = Twython(consumer_key,
+                  consumer_secret,
+                  access_token,
+                  access_token_secret
+                  )
+
+# Checking if there's an argument for where to log.
+if len(sys.argv) == 1:
+    path = 'logs/'
+else:
+    path = str(sys.argv[1])
+
+makeDir(path)
+
+logging.basicConfig(filename=(path+'record.log'), level=logging.INFO,
+                    format='%(asctime)s %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
+
+message = markov.message()
+
+try:
+    twitter.update_status(status=message)
+    logging.info('I Tweeted!\n')
+except Exception as err:
+    # This is mostly to catch 405 forbidden's on duplicate tweets
+    logging.info(str(err)+'\n')
+
+exit()
