62ca99fdb94e08f7013871f90742260922e17d38
Nick Kelly <importantnk@gmail.com>
2017-09-19 12:50:22 -0400

    Pirate day! Argh!

--- markov.py
+++ markov.py
@@ -12,4 +12,4 @@ def message():
     text_model = markovify.Text(text)
 
     # Print three randomly-generated sentences of no more than 140 characters
-    return text_model.make_short_sentence(140)
+    return text_model.make_short_sentence(135) + " Arr!"
